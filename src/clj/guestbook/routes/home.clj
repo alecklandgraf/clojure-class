b7156d87b4e2b93c32887e738a99106a260a53a1
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-02-17 00:23:31 -0800

    adding home

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,17 +1,19 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"
+    {:messages (db/get-messages)} ))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
